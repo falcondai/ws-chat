5e5fdad6cef5051187b33dced1a0dde329149931
Falcon Dai <me@falcondai.com>
2013-08-17 01:25:29 -0500

    migrated index.html to index.jade

--- app.js
+++ app.js
@@ -1,62 +1,67 @@
 var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server);
 
 server.listen(9000);
 console.log('start chatting at: %s', 'http://localhost:9000/');
 
 app.use(express.logger());
+
+// template config
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+  res.render('index.jade');
 });
 
 io.of('/chat').on('connection', function(socket) {
-    // welcome message
-    socket.emit('new feed', {
-        body: 'connected to Chatty!'
-    });
+  // welcome message
+  socket.emit('new feed', {
+    body: 'connected to Chatty!'
+  });
 
-    socket.on('join', function (data) {
-        socket.set('name', data.name, function () {
-            socket.broadcast.emit('new member', {
-                id: socket.id,
-                name: data.name
-            });
-        });
+  socket.on('join', function (data) {
+    socket.set('name', data.name, function () {
+      socket.broadcast.emit('new member', {
+        id: socket.id,
+        name: data.name
+      });
     });
+  });
 
-    // Google Wave-like partial message
-    socket.on('partial', function (data) {
-        socket.get('name', function (err, name) {
-            socket.broadcast.emit('new partial', {
-                id: socket.id,
-                sender: name,
-                body: data.body
-            });
-        });
+  // Google Wave-like partial message
+  socket.on('partial', function (data) {
+    socket.get('name', function (err, name) {
+      socket.broadcast.emit('new partial', {
+        id: socket.id,
+        sender: name,
+        body: data.body
+      });
     });
+  });
 
-    socket.on('send', function (data) {
-        socket.get('name', function (err, name) {
-            socket.broadcast.emit('new message', {
-                id: socket.id,
-                sender: name,
-                body: data.body
-            });
-        });
+  socket.on('send', function (data) {
+    socket.get('name', function (err, name) {
+      socket.broadcast.emit('new message', {
+        id: socket.id,
+        sender: name,
+        body: data.body
+      });
     });
+  });
 
-    socket.on('disconnect', function () {
-        socket.get('name', function (err, name) {
-            socket.broadcast.emit('member disconnected', {
-                id: socket.id,
-                name: name
-            });
-        });
+  socket.on('disconnect', function () {
+    socket.get('name', function (err, name) {
+      socket.broadcast.emit('member disconnected', {
+        id: socket.id,
+        name: name
+      });
     });
+  });
 
-    socket.on('anything', function (data) {
-        console.log('unreserved message: ', data);
-    });
+  socket.on('anything', function (data) {
+    console.log('unreserved message: ', data);
+  });
 });
